a46bd47ef3d8d0dea5ee7679b025aac666edd3c4
Yunus Emre Kaymak <y.e.imp@hotmail.com>
2024-07-20 18:25:03 +0300

    Initialize project

---
+++ src/main/java/com/yunus/stock_notification_app/StockNotificationAppApplication.java
@@ -0,0 +1,13 @@
+package com.yunus.stock_notification_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockNotificationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockNotificationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunus/stock_notification_app/StockNotificationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.stock_notification_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockNotificationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
