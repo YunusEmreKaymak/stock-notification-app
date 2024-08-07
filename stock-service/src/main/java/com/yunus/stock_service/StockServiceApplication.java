ba459d62a24f5107fb1be4d32849da5afd7172e6
Yunus Emre Kaymak <y.e.imp@hotmail.com>
2024-08-07 21:25:16 +0300

    feat: configure alert-service and stock-service as eureka clients

--- stock-service/src/main/java/com/yunus/stock_service/StockServiceApplication.java
+++ stock-service/src/main/java/com/yunus/stock_service/StockServiceApplication.java
@@ -1,16 +1,15 @@
 package com.yunus.stock_service;
 
-import com.yunus.stock_service.kafka.KProducer;
-import com.yunus.stock_service.kafka.KProcessor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.net.URISyntaxException;
 import java.util.concurrent.ExecutionException;
 
-import static com.yunus.stock_service.stock.FetchStock.getStockData;
-
 @SpringBootApplication
+@EnableFeignClients
 public class StockServiceApplication {
     @Value("${alphavantage.api.key}")
     private static String apiKey;
@@ -21,11 +20,12 @@ public class StockServiceApplication {
     @Value("${kafka.config.topic}")
     private static String topic;
 
-    private static String stockName = "IBM";
+    private static final String stockName = "IBM";
 
     public static void main(String[] args) throws InterruptedException, URISyntaxException, ExecutionException {
-        String stockHistory = getStockData(stockName, apiKey);
-        KProcessor.stream(bootstrapServers, topic);
-        KProducer.producer(stockHistory, bootstrapServers, topic);
+        SpringApplication.run(StockServiceApplication.class, args);
+//        String stockHistory = getStockData(stockName, apiKey);
+//        KProcessor.stream(bootstrapServers, topic);
+//        KProducer.producer(stockHistory, bootstrapServers, topic);
     }
 }
