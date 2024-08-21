b1c06d77423bc481d6f2ccae4554c38b1727cbf9
Yunus Emre Kaymak <y.e.imp@hotmail.com>
2024-08-21 20:26:56 +0300

    refactor: extract kafka methods to application runner

---
+++ stock-service/src/main/java/com/yunus/stock_service/AppRunner.java
@@ -0,0 +1,53 @@
+package com.yunus.stock_service;
+
+import com.yunus.stock_service.client.AlertServiceClient;
+import com.yunus.stock_service.dto.AlertDto;
+import com.yunus.stock_service.kafka.KProcessor;
+import com.yunus.stock_service.kafka.KProducer;
+import com.yunus.stock_service.stock.FetchStock;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+    @Value("${alphavantage.api.key}")
+    private static String apiKey;
+
+    @Value("${kafka.config.bootstrapServers}")
+    private static String bootstrapServers;
+
+    @Value("${kafka.config.topic}")
+    private static String topic;
+
+    private final AlertServiceClient alertServiceClient;
+
+
+    public AppRunner(AlertServiceClient alertServiceClient) {
+        this.alertServiceClient = alertServiceClient;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws URISyntaxException, ExecutionException, InterruptedException {
+        checkAlert(alertServiceClient);
+    }
+
+
+    private void checkAlert(AlertServiceClient alertServiceClient) throws URISyntaxException, ExecutionException, InterruptedException {
+        List<AlertDto> alerts = alertServiceClient.getAllAlerts();
+        for (AlertDto alert : alerts) {
+            String stockData = FetchStock.getStockData(alert.getStockName(), apiKey);
+            streamAlert(stockData);
+        }
+    }
+
+    private void streamAlert(String stockData) throws InterruptedException {
+        KProcessor.stream(bootstrapServers, topic);
+        KProducer.producer(stockData, bootstrapServers, topic);
+    }
+}
--- stock-service/src/main/java/com/yunus/stock_service/StockServiceApplication.java
+++ stock-service/src/main/java/com/yunus/stock_service/StockServiceApplication.java
@@ -1,31 +1,14 @@
 package com.yunus.stock_service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import java.net.URISyntaxException;
-import java.util.concurrent.ExecutionException;
-
 @SpringBootApplication
 @EnableFeignClients
 public class StockServiceApplication {
-    @Value("${alphavantage.api.key}")
-    private static String apiKey;
 
-    @Value("${kafka.config.bootstrapServers}")
-    private static String bootstrapServers;
-
-    @Value("${kafka.config.topic}")
-    private static String topic;
-
-    private static final String stockName = "IBM";
-
-    public static void main(String[] args) throws InterruptedException, URISyntaxException, ExecutionException {
+    public static void main(String[] args) {
         SpringApplication.run(StockServiceApplication.class, args);
-//        String stockHistory = getStockData(stockName, apiKey);
-//        KProcessor.stream(bootstrapServers, topic);
-//        KProducer.producer(stockHistory, bootstrapServers, topic);
     }
 }
