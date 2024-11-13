c835ad6631b1f28f28edf5efa884c053905924d3
Yunus Emre Kaymak <y.e.imp@hotmail.com>
2024-11-13 22:47:15 +0300

    feat: add stock-persistence-service

---
+++ stock-persistence-service/src/main/java/com/yunus/stock_persistence_service/StockPersistenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yunus.stock_persistence_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPersistenceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockPersistenceServiceApplication.class, args);
+    }
+
+}
---
+++ stock-persistence-service/src/test/java/com/yunus/stock_persistence_service/StockPersistenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.stock_persistence_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPersistenceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
