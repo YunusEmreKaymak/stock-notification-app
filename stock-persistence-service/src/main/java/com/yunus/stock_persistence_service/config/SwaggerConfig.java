7a5fbf995d8710da84021b75b56868e9f3687603
Yunus Emre Kaymak <y.e.imp@hotmail.com>
2024-12-10 20:16:42 +0300

    feat: add swagger config

---
+++ stock-persistence-service/src/main/java/com/yunus/stock_persistence_service/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.yunus.stock_persistence_service.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+//http://localhost:8083/swagger-ui/index.html#
+@OpenAPIDefinition
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(
+            @Value("${spring.application.name}") String title,
+            @Value("${spring.application.description}") String description,
+            @Value("${spring.application.version}") String version
+    ) {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title(title)
+                                .description(description)
+                                .version(version)
+                );
+    }
+}
